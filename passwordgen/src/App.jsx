f7ed2e2e4bace19b91bb7097e81f947e3bdcf827
Hashim Anhad <anshadanchu368@gmail.com>
2024-09-09 12:55:17 +0530

    password set

---
+++ passwordgen/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-4xl text-slate-100">password generator</h1>
+
+    </>
+  )
+}
+
+export default App
